44078c78efbf2959de6b9bc628195bbd8e047901
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-13 23:09:49 +0800

    4.1 Functional Components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function greet(name) {
-  return <h1>Hello, {name || 'Kisanak'}!</h1>
+function Greet(props) {
+  return <h1>Hello, {props.name || 'Kisanak'}!</h1>
 }
 
 ReactDOM.render(
-  greet('Pejuang'),
+  <Greet name='Pejuang' />,
   document.getElementById('root')
 )
